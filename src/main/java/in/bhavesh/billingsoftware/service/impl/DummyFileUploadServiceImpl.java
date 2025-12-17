aa58aaf293f635925e3e342e0a8554d9ec8ed9a9
Bhavesh <vinodmangal2002@gmail.com>
2025-12-17 16:43:28 +0530

    Mark prod FileUploadService as primary

--- src/main/java/in/bhavesh/billingsoftware/service/impl/DummyFileUploadServiceImpl.java
+++ src/main/java/in/bhavesh/billingsoftware/service/impl/DummyFileUploadServiceImpl.java
@@ -1,4 +1,23 @@
 package in.bhavesh.billingsoftware.service.impl;
 
-public class DummyFileUploadServiceImpl {
+import in.bhavesh.billingsoftware.service.FileUploadService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Profile("prod")
+@Primary
+public class DummyFileUploadServiceImpl implements FileUploadService {
+
+    @Override
+    public String uploadFile(MultipartFile file) {
+        return null; // prod mein upload disabled
+    }
+
+    @Override
+    public boolean deleteFile(String imgUrl) {
+        return true;
+    }
 }
