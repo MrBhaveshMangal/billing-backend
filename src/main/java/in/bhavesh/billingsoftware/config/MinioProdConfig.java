6d321adae2dfa7b7127bc93c7fb223674afcc03a
Bhavesh <vinodmangal2002@gmail.com>
2025-12-18 10:36:23 +0530

    Add Minio client config for prod profile

---
+++ src/main/java/in/bhavesh/billingsoftware/config/MinioProdConfig.java
@@ -0,0 +1,29 @@
+package in.bhavesh.billingsoftware.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class MinioProdConfig {
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
